a0637ff34f84bbf2ece372eb0641a7a866cd5b4d
Pankratov <pnrf@yandex.ru>
2023-02-06 21:10:05 +0300

    feat: add block intro

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,41 @@
 import '../styles/index.css';
+
+import Swiper, { Navigation, Pagination } from 'swiper';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+// ------------ swiper (slider)------------
+
+const swiperSectionIntro = new Swiper(".swiper_section_intro", {
+  modules: [Navigation, Pagination],
+
+  direction: 'horizontal',
+  loop: true,
+  loopFillGroupWithBlank: true,
+
+  navigation: {
+    prevEl: '.swiper__button-prev_section_intro',
+    nextEl: '.swiper__button-next_section_intro',
+  },
+
+  pagination: {
+    el: ".swiper-pagination_section_intro",
+    type: 'bullets',
+    clickable: true,
+  },
+
+  // breakpoints: {
+  //   320: {
+  //     slidesPerView: 1,
+  //     spaceBetween: 10,
+  //     slidesPerGroup: 1,
+  //   },
+
+  //   678: {
+  //     slidesPerView: 2,
+  //     spaceBetween: 20,
+  //     slidesPerGroup: 2,
+  //   }
+  // }
+});
